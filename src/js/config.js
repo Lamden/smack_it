f068b1876bd523858b17936e76a3750bb4b99c16
jeffwscott <jeffscott@outlook.com>
2020-10-07 18:28:40 -0400

    https

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ export const config = {
     smartcontact: 'con_smack_that',
     currencySymbol: "TAU",
     blockExplorer: "https://mainnet.lamden.io",
-    masternodes: ["http://masternode-01.lamden.io", "http://masternode-02.lamden.io"],
+    masternodes: ["https://masternode-01.lamden.io", "https://masternode-02.lamden.io"],
     cost: 10,
     txFee: 1
 }
